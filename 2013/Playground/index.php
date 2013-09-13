97d538808277d651405647195efd9edccc2028f3
unknown <anthony@.(none)>
2013-09-12 21:46:44 -0400

    code cleaned

--- 2013/Playground/index.php
+++ 2013/Playground/index.php
@@ -1,66 +1,117 @@
-<? 
-
-	$location = 'home';
-	
-	$pages = array(
-		'home' => array(
-			'url' => 'index.php')
-			);
-			
-
-?>
 
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 template</title>
+    <title>Bootstrap 101 Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
-    <![endif]-->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" />
+    <style type="text/css">
+    	body { padding-top: 70px; }
+    </style>
   </head>
+  
+  
   <body>
-    <h1>This is the Playground</h1>
-
-	<div class="row">
-  		<div class="col-xs-12 col-md-8">.col-xs-12 col-md-8</div>
-  		<div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-	</div>
-
-	<!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
-	<div class="row">
-	  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-	  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-	  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-	</div>
-
-	<!-- Columns are always 50% wide, on mobile and desktop -->
-	<div class="row">
-	  <div class="col-xs-6">.col-xs-6</div>
-	  <div class="col-xs-6">.col-xs-6</div>
-	</div>
-
-
-
-
-
-    <script src="//code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
+  	   		<div class="container">
+   				 
+   				 <div class="navbar navbar-default" role="navigation">
+   					
+   					<div class="navbar-header">
+						
+						<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".nav-c">
+			    			<span class="icon-bar"></span>
+			        		<span class="icon-bar"></span>
+			        		<span class="icon-bar"></span>
+			    		</button>
+		      	
+		      			<a class="navbar-brand" href="#">Playground</a>
+					</div>
+		
+		
+					<div class="collapse navbar-collapse nav-c">
+			            <ul class="nav navbar-nav">
+			              <li class="home active"><a href="./">Home</a></li>
+			              <li class="contact "><a href="contact.php">Contact</a></li>
+			              <li class="links "><a href="links.php">Links</a></li>
+							
+						  <li class="dropdown">
+						        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+						        <ul class="dropdown-menu">
+						          <li><a href="#">Action</a></li>
+						          <li><a href="#">Another action</a></li>
+						          <li><a href="#">Something else here</a></li>
+						          <li><a href="#">Separated link</a></li>
+						          <li><a href="#">One more separated link</a></li>
+						        </ul>
+						   </li>
+		      			</ul>
+		      			
+						<p class="navbar-text pull-right">Signed in as <a href="#" class="navbar-link">Anthony Turner</a></p>
+					</div>
+				</div>
+			</div>
+		  	
+		  	<div class="container">
+		 		 	<pre>
+		  				Array(
+	    						[home] => Array(
+	            									[url] => index.php
+	            									[section] => home
+	            									[title] => Home
+	        									)
+	
+	    						[links] => links.php
+	    						[contact] => contact.php
+							)
+	  		
+				  	</pre>
+			  	
+	  				<div class="jumbotron">
+			    		<h1>Hello Home</h1>
+			    
+				    	<p>Welcome class of 2013 to Web Server Programming</p>
+	  					<a class="btn btn-lg btn-success">Learn More</a>
+	  				</div>
+	  		
+	  				<div class="row">
+	  					
+	  					<div class="col-sm-6 col-md-4">
+				  			<h2>Heading</h2>
+		          			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		          			<p><a class="btn btn-primary" href="#">View details »</a></p>
+						</div>
+	   				
+	   					<div class="col-sm-6 col-md-4">
+		          
+		          			<h2>Important Points</h2>
+		          			<ul>
+		          				<li>The three main links in the navbar work</li>
+		          				<li>They are all centralized in one file</li>
+		          				<li>They change apearance to show you which page you are on.</li>
+		          				<li>These colunms start as three columns then reduce as the browser shrinks</li>
+				  			</ul>
+		          			<p><a class="btn btn-primary" href="#">View details »</a></p>
+						</div>
+				
+	 					<div class="col-sm-6 col-md-4">
+		          			<h2>Heading</h2>
+		          			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		          			<p><a class="btn btn-primary" href="#">View details »</a></p>
+	  					</div>
+	  				</div>
+  			</div>
+  	
+  	
+    <script src="http://code.jquery.com/jquery.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-    <script type="text/javascript"></script>
-    	$(function()){<!-- Find every single item that has the ".home" class on it and highlight it -->
-    		
-    		
-    		$(".home").addClass("active");
-    		
-    		
-    	}
-    
-    
-  </body>
+ </body>
+ 
+ 
 </html>
+
+
+
+
+
+
+
