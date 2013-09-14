0c3c6b198e4d53a9a794487ba7a2fe823bdb3239
Anthony Turner <enciphered_bits@yahoo.com>
2013-09-13 22:15:11 -0400

    set nav-bar to a fixed type.

--- 2013/Playground/contact.php
+++ 2013/Playground/contact.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 Template</title>
+    <title>Bootstrap 101 Template - Contact</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" />
     <style type="text/css">
@@ -12,10 +12,10 @@
   
   <body>
   	   	<!-- Navigation bar -->
-  	   		<div class="container">
-   				 
-   				 <div class="navbar navbar-inverse" role="navigation">
-   					
+  	   		
+  	   		<div class="navbar navbar-inverse navbar-fixed-top" role="navigation" >
+
+  	   			<div class="container">
    					<div class="navbar-header">
 						
 						<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".nav-c">
@@ -25,15 +25,17 @@
 			    		</button>
 		      	
 		      			<a class="navbar-brand" href="#">Playground</a>
-					</div>
+					</div><!-- End of navbar-header-->
 		
 		
 					<div class="collapse navbar-collapse nav-c">
 			            <ul class="nav navbar-nav">
+			            	<!-- Simple static links -->
 			              <li class="home"><a href="index.php">Home</a></li>
 			              <li class="contact active"><a href="contact.php">Contact</a></li>
 			              <li class="links "><a href="links.php">Links</a></li>
 							
+							<!-- dropdown list area -->
 						  <li class="dropdown">
 						        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
 						        <ul class="dropdown-menu">
@@ -47,17 +49,17 @@
 		      			</ul>
 		      			
 						<p class="navbar-text pull-right">Signed in as <a href="#" class="navbar-link">Anthony Turner</a></p>
-					</div>
-				</div>
-			</div>
-
-			<!-- main -->
+					</div><!-- End of collapse navbar-->
+				</div><!-- End of container -->
+			</div><!-- End of navbar-->
+			
+			<!-- main area -->
 			
 				<div class="container">
 					<div class="well">
 
 						<h1>Contact Info</h1>
-						<p>I can be reached through the below form. <a class="btn btn-default "> More info </a></p>
+						<p>I can be reached by filling out the below form. <a class="btn btn-default "> More info </a></p>
 							
 					</div>
 					
@@ -66,6 +68,8 @@
 							
 								<h2> Contact me:</h2>
 								<form class="form-horizontal">
+								
+									<!-- Input box for email address -->
 									<div class="form-group">
 										<label for="input_email1" class="col-md-2 control-label">Your email</label>
 										<div class="col-md-10">
@@ -74,6 +78,7 @@
 										</div>
 									</div>
 									
+									<!-- Input area for composed message -->
 									<div class="form-group">
 										<label for="txt_msg" class="col-md-2 control-label">Message</label>
 										<div class="col-md-10">
@@ -82,7 +87,7 @@
 										</div>
 									</div>
 										
-										
+										<!-- Submit button -->
 									<div class="form-group">
 										<div class="col-md-offset-2 col-md-10">
 											
