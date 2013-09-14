194b983620eb009bcd61f7db2ee6ae6c3f0dd573
unknown <anthony@.(none)>
2013-09-14 00:01:10 -0400

    Removed footer

--- 2013/Playground/links.php
+++ 2013/Playground/links.php
@@ -119,12 +119,6 @@
 					
 				</div><!-- End container -->
 					
-					<div id="footer">
-      <div class="container">
-        <p class="text-muted credit">Example courtesy <a href="http://martinbean.co.uk">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
-      </div>
-    </div>
-					
 
 	<script src="http://code.jquery.com/jquery.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
