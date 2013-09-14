2ff26eb9136c2239e1be35a9672dc12c600fb0f5
Anthony Turner <enciphered_bits@yahoo.com>
2013-09-13 22:14:34 -0400

    set nav-bar to a fixed type.

--- 2013/Playground/index.php
+++ 2013/Playground/index.php
@@ -14,11 +14,10 @@
   <body>
   	   	
   	   	
-  	   	<!-- Navigation bar -->
-  	   		<div class="container">
-   				 
-   				 <div class="navbar navbar-inverse" role="navigation">
-   					
+  	   	<!-- Navigation bar ~ using a fixed navbar that keeps showing when scrolling down and up-->
+  	   		<div class="navbar navbar-inverse navbar-fixed-top" role="navigation" >
+
+  	   			<div class="container">
    					<div class="navbar-header">
 						
 						<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".nav-c">
@@ -28,15 +27,17 @@
 			    		</button>
 		      	
 		      			<a class="navbar-brand" href="#">Playground</a>
-					</div>
+					</div><!-- End of navbar-header-->
 		
 		
 					<div class="collapse navbar-collapse nav-c">
 			            <ul class="nav navbar-nav">
-			              <li class="home active"><a href="./">Home</a></li>
+			            	<!-- Simple static links -->
+			              <li class="home active"><a href="index.php">Home</a></li>
 			              <li class="contact "><a href="contact.php">Contact</a></li>
 			              <li class="links "><a href="links.php">Links</a></li>
 							
+							<!-- dropdown list area -->
 						  <li class="dropdown">
 						        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
 						        <ul class="dropdown-menu">
@@ -50,10 +51,11 @@
 		      			</ul>
 		      			
 						<p class="navbar-text pull-right">Signed in as <a href="#" class="navbar-link">Anthony Turner</a></p>
-					</div>
-				</div>
-			</div>
-		  	
+					</div><!-- End of collapse navbar-->
+				</div><!-- End of container -->
+			</div><!-- End of navbar-->
+			
+			<!-- main area -->
 		  	<div class="container">
 		 		 	<pre>
 		  				Array(
@@ -101,6 +103,37 @@
 		          			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
 		          			<p><a class="btn btn-primary" href="#">View details »</a></p>
 	  					</div>
+	  					
+	  					
+	  				</div>
+	  				
+	  				<div class="row">
+	  					
+	  					<div class="col-sm-6 col-md-4">
+				  			<h2>Heading</h2>
+		          			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		          			<p><a class="btn btn-primary" href="#">View details »</a></p>
+						</div>
+	   				
+	   					<div class="col-sm-6 col-md-4">
+		          
+		          			<h2>More Important Points</h2>
+		          			<ul>
+		          				<li>The three main links in the navbar work</li>
+		          				<li>They are all centralized in one file</li>
+		          				<li>They change apearance to show you which page you are on.</li>
+		          				<li>These colunms start as three columns then reduce as the browser shrinks</li>
+				  			</ul>
+		          			<p><a class="btn btn-primary" href="#">View details »</a></p>
+						</div>
+				
+	 					<div class="col-sm-6 col-md-4">
+		          			<h2>Heading</h2>
+		          			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		          			<p><a class="btn btn-primary" href="#">View details »</a></p>
+	  					</div>
+	  					
+	  					
 	  				</div>
   			</div>
   	
