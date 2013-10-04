d5360e50d42a4e2bd686b93040a856a1c6e0e9ac
unknown <IMSSTUD@.admin.newpaltz.edu>
2013-10-04 14:05:28 -0400

    MySQL connection setup

--- 2013/Final/index.php
+++ 2013/Final/index.php
@@ -1,75 +1,50 @@
 <? include 'inc/_global.php'; ?>
 
+<div class="well">
 
 <? 
 
-
 	$conn = GetConnection();
-	$result = $conn->query('SELECT * FROM Keywords');
-	$rs = $result->fetch_assoc();
 	
-
+	$result = $conn-> query('SELECT * FROM 2013Fall_Users');
+    
+    	 /* fetch associative array */
+   		 while ($rs = $result->fetch_assoc()) {
+       		 printf ("%s (%s)\n", $rs["FirstName"], $rs["LastName"]);
+			
+   		 }
 ?>
 
-<!DOCTYPE html>
+</div>
 
+	
+<!DOCTYPE html>
 <html>
   <head>
-    <title>Final page</title>
+    <title>Bootstrap 101 Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
-    <![endif]-->
   </head>
   <body>
-    <h1>This page is the index.php for the Final folder</h1>
-  
-    <? 
+    <h1>This is the final</h1>
     
-    	$msg = 'Hello ' ;
-		$name = ' Anthony' ;
-	
+    <?
+    
+    
+		
+		
+		$msg = 'Hello ';
+		$name = 'Anthony';
 		include 'something.php';
-    
-    ?>
-    
-    <div>
-    	<style>
-   		 .row.div(background-color: #FFCCCC);
-    
-    	</style>
+		
+		
+	?>
+
+	<span class="label label-success"><?= $msg . $name ?></span>
 	
-	    
-		<div class="row">
-		  		<div class="col-xs-12 col-md-8">.col-xs-12 col-md-8</div>
-		  		<div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-			</div>
-		
-			<!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
-			<div class="row">
-			  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-			  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-			  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-			</div>
-		
-			<!-- Columns are always 50% wide, on mobile and desktop -->
-			<div class="row">
-			  <div class="col-xs-6">.col-xs-6</div>
-			  <div class="col-xs-6">.col-xs-6</div>
-			</div>
-
-
-	</div>
-
-
-
     <script src="//code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-   <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
   </body>
 </html>
