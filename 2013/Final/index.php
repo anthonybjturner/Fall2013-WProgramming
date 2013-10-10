9489549fce0beeeef7011af684b24088ab76b1f5
unknown <turnera1@.active.newpaltz.edu>
2013-10-10 15:08:31 -0400

    Adjustments to final

--- 2013/Final/index.php
+++ 2013/Final/index.php
@@ -3,51 +3,35 @@
   <head>
     <title>Final page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
-    <![endif]-->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" />
+    <style type="text/css">
+    	body { padding-top: 70px; }
+    	h1{ text-align: center }
+    </style>
   </head>
+  
   <body>
     <h1>This page is the index.php for the Final folder</h1>
   
     
-    <div>
-    	<style>
-   		 .row.div(background-color: #FFCCCC);
+    <? include('_nav.php'); ?> 
+  	   
+			<!-- main area -->
+		 <div class="container">
+		 		 	
+			  	
+	  		<div class="jumbotron">
+			    
+				    	
+  			</div><!-- End container -->
+  	
+
+		</div>
+
+
+  	 <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script src="Scripts/main.js"></script>
     
-    	</style>
-	
-	    
-		<div class="row">
-		  		<div class="col-xs-12 col-md-8">.col-xs-12 col-md-8</div>
-		  		<div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-			</div>
-		
-			<!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
-			<div class="row">
-			  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-			  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-			  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-			</div>
-		
-			<!-- Columns are always 50% wide, on mobile and desktop -->
-			<div class="row">
-			  <div class="col-xs-6">.col-xs-6</div>
-			  <div class="col-xs-6">.col-xs-6</div>
-			</div>
-
-
-	</div>
-
-
-
-    <script src="//code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-   <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
   </body>
 </html>
